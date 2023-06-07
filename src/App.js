597ba36760df0a4ea718f9b1537ac8be5a780a91
jiayul5339 <98488999+jiayul5339@users.noreply.github.com>
2023-06-07 17:22:14 -0400

    Adding custom React Hooks for window size and axios fetching

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns'
 import api from './api/post';
+import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,27 +21,13 @@ function App() {
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
+  const { width } = useWindowSize();
+
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        // axios will automatically catch errors
-        const response = await api.get('/posts');
-        setPosts(response.data)
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+    setPosts(data);
+  }, [data]);
 
   useEffect(() => {
     const filteredResults = posts.filter(post => 
@@ -95,9 +83,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout 
         search={search}
-        setSearch={setSearch}/>}>
+        setSearch={setSearch}
+        width={width}/>}>
 
-        <Route index element={<Home posts={searchResults} />}/>
+        <Route index element={<Home 
+          posts={searchResults}
+          fetchError={fetchError}
+          isLoading={isLoading} />}/>
         <Route path="post">
           <Route index element={<NewPost 
             handleSubmit={handleSubmit}
---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup function that removes event listener, prevents memory leaks
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, [])
+
+    return windowSize;
+}
+
+export default useWindowSize;
